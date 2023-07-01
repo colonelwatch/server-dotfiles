da355aa9e16cbc6fa15892e12408c983939e7727
Kenny Peng <53552802+colonelwatch@users.noreply.github.com>
2023-06-30 19:07:57 -0700

    Add nodered service

--- bootstrap.sh
+++ bootstrap.sh
@@ -47,7 +47,11 @@ sudo systemctl restart NetworkManager
 # <USER>
 
 sudo apt install -y \
-    fish rclone rsync vim
+    build-essential curl fish mosquitto rclone rsync vim
+
+wget https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered
+bash ./update-nodejs-and-nodered --confirm-install --skip-pi --no-init --node18
+rm ./update-nodejs-and-nodered
 
 # install config files
 mkdir -p ~/.config
--- recovery.sh
+++ recovery.sh
@@ -5,3 +5,7 @@ rclone config reconnect server_bak: --auto-confirm
 rclone config reconnect laptop_bak: --auto-confirm
 rclone copy server_bak: ~ -P --fast-list --checkers=32 --transfers=16
 rclone copy laptop_bak: ~/Laptop -P --fast-list --checkers=32 --transfers=16
+
+# nodered config was contained in the rclone backup
+sudo systemctl enable nodered
+sudo systemctl start nodered
