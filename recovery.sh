571f2a9fc07a982aec3d5f4f03cc571d3c65e7bf
Kenny Peng <53552802+colonelwatch@users.noreply.github.com>
2023-07-23 17:53:08 -0700

    Add nodered dependency manually

--- recovery.sh
+++ recovery.sh
@@ -6,6 +6,8 @@ rclone config reconnect laptop_bak: --auto-confirm
 rclone copy server_bak: ~ -P --fast-list --checkers=32 --transfers=16
 rclone copy laptop_bak: ~/Laptop -P --fast-list --checkers=32 --transfers=16
 
+npm install node-red-contrib-smartnora # just add smartnora manually
+
 # nodered config was contained in the rclone backup
 sudo systemctl enable nodered
 sudo systemctl start nodered
