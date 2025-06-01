fb7b266804a07c6689c0422eb91df8a48ede2be7
Kenny Peng <53552802+colonelwatch@users.noreply.github.com>
2025-06-01 13:49:55 -0700

    Add a yes arg to the conda update

--- bootstrap.sh
+++ bootstrap.sh
@@ -103,7 +103,7 @@ function get_nodered {
 function get_miniconda {
     if [ -d ~/miniconda3 ]; then
         source ~/miniconda3/bin/activate
-        conda update -n base -c defaults conda
+        conda update -y -n base -c defaults conda
         return 0
     fi 
 
